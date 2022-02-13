ff39268c68f4ed892056fdadbb22e1e992d738a8
Mmackz <matthewmackenz@gmail.com>
2022-02-12 16:57:59 -0800

    ✨ add legend

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const width = 800;
 const paddingY = 60;
 const paddingX = 100;
 
+const color = d3.scaleOrdinal(d3.schemeSet2);
+
 const chartContainer = d3.select(".chart");
 
 chartContainer
@@ -26,13 +28,12 @@ const chart = chartContainer
 d3.json(
    "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
 ).then((data) => {
-   console.log(data);
+   // console.log(data);
 
    // Race Times (y-axis)
    const timeFormat = "%M:%S";
    const parsedTime = data.map((data) => d3.timeParse(timeFormat)(data.Time));
    const yAxisScale = d3.scaleTime().domain(d3.extent(parsedTime)).range([0, height]);
-   console.log(yAxisScale(d3.timeParse(timeFormat)("39:23")))
    const yAxis = d3.axisLeft(yAxisScale).tickFormat((d) => d3.timeFormat(timeFormat)(d));
 
    chart
@@ -45,7 +46,6 @@ d3.json(
    const maxYear = d3.max(data, (d) => d.Year + 1);
    const minYear = d3.min(data, (d) => d.Year - 1);
    const xAxisScale = d3.scaleLinear().domain([minYear, maxYear]).range([0, width]);
-   console.log(xAxisScale("2015"))
    const xAxis = d3.axisBottom(xAxisScale).tickFormat((d) => d3.timeParse(d));
    chart
       .append("g")
@@ -54,16 +54,41 @@ d3.json(
       .attr("transform", `translate(${paddingY}, ${height + 20})`);
 
    // Data points
-   
+
    d3.select("svg")
       .selectAll("circle")
       .data(data)
       .enter()
       .append("circle")
       .attr("class", "dot")
-      .attr("cx", d => xAxisScale(d.Year) + 60)
-      .attr("data-xvalue", d => d.Year)
-      .attr("data-yvalue", d => d3.timeParse(timeFormat)(d.Time))
-      .attr("cy", d => yAxisScale(d3.timeParse(timeFormat)(d.Time)) + 20)
-      .attr("r", 5)
+      .attr("cx", (d) => xAxisScale(d.Year) + 60)
+      .attr("data-xvalue", (d) => d.Year)
+      .attr("data-yvalue", (d) => d3.timeParse(timeFormat)(d.Time))
+      .attr("cy", (d) => yAxisScale(d3.timeParse(timeFormat)(d.Time)) + 20)
+      .attr("r", 6)
+      .attr("fill", (d) => color(Boolean(d.Doping)));
+
+   const legendContainer = chart.append("g").attr("id", "legend");
+
+   const legend = legendContainer
+      .selectAll("#legend")
+      .data(color.domain())
+      .enter()
+      .append("g")
+      .attr("transform", (d, i) => `translate(0, ${height / 2 - i * 20})`);
+
+   legend
+      .append("rect")
+      .attr("class", "legend-label")
+      .attr("x", width)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+   legend
+      .append("text")
+      .attr("class", "label-text")
+      .attr("x", width - 6)
+      .attr("y", 12)
+      .text((d) => `${d ? "No" : "Riders with"} doping allegations`);
 });
